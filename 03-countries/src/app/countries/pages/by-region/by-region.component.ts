eba33b24dee45cf45ec207b9d32e751f88ba7775
Juan Vanegas <eatteer@gmail.com>
2023-01-16 20:29:11 -0500

    Implement countries filtering by region

--- 03-countries/src/app/countries/pages/by-region/by-region.component.ts
+++ 03-countries/src/app/countries/pages/by-region/by-region.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/countries.interfaces';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-by-region',
   templateUrl: './by-region.component.html',
   styles: [],
 })
-export class ByRegionComponent {}
+export class ByRegionComponent {
+  public regions: string[] = [
+    'africa',
+    'americas',
+    'asia',
+    'europe',
+    'oceania',
+  ];
+  public activeRegion: string = '';
+  public countries: Country[] = [];
+
+  public constructor(private countriesService: CountriesService) {}
+
+  public setActiveRegion(region: string): void {
+    if (region === this.activeRegion) return;
+    this.activeRegion = region;
+    this.countries = [];
+    this.countriesService.searchByRegion(region).subscribe((countries) => {
+      this.countries = countries;
+    });
+  }
+
+  public styleButton(region: string): string {
+    return region !== this.activeRegion ? 'btn-outline-primary' : 'btn-primary';
+  }
+}
--- 03-countries/src/app/countries/services/countries.service.ts
+++ 03-countries/src/app/countries/services/countries.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { Country } from '../interfaces/countries.interfaces';
 
@@ -9,16 +9,19 @@ import { Country } from '../interfaces/countries.interfaces';
 export class CountriesService {
   private BASE_URL: string = 'https://restcountries.com/v3.1';
 
+  private fields: string = 'flags,name,population,cca2';
+  private params: HttpParams = new HttpParams().set('fields', this.fields);
+
   public constructor(private httpClient: HttpClient) {}
 
   public searchByName(name: string): Observable<Country[]> {
     const endpoint: string = `${this.BASE_URL}/name/${name}`;
-    return this.httpClient.get<Country[]>(endpoint);
+    return this.httpClient.get<Country[]>(endpoint, { params: this.params });
   }
 
   public searchByCapital(capital: string): Observable<Country[]> {
     const endpoint: string = `${this.BASE_URL}/capital/${capital}`;
-    return this.httpClient.get<Country[]>(endpoint);
+    return this.httpClient.get<Country[]>(endpoint, { params: this.params });
   }
 
   public searchByCode(code: string): Observable<Country> {
@@ -26,5 +29,10 @@ export class CountriesService {
     return this.httpClient
       .get<Country[]>(endpoint)
       .pipe(map((countries) => countries[0]));
+  }
+
+  public searchByRegion(region: string): Observable<Country[]> {
+    const endpoint: string = `${this.BASE_URL}/region/${region}`;
+    return this.httpClient.get<Country[]>(endpoint, { params: this.params });
   }
 }
