45812d3e44f4e68f944f44bc135e162cab292215
Practicante Programacion TI <practicante.ti2@austinreed.com.co>
2023-01-17 07:59:36 -0500

    Implement search suggestions on search by country page

--- 03-countries/src/app/countries/pages/by-country/by-country.component.ts
+++ 03-countries/src/app/countries/pages/by-country/by-country.component.ts
@@ -10,13 +10,16 @@ import { Country } from '../../interfaces/countries.interfaces';
 export class ByCountryComponent {
   public term: string = '';
   public countries: Country[] = [];
+  public suggestions: Country[] = [];
   public error: boolean = false;
+  public showSuggestions: boolean = false;
 
   public constructor(private countriesService: CountriesService) {}
 
   public submit(term: string): void {
     this.term = term;
     this.error = false;
+    this.showSuggestions = false;
     this.countriesService.searchByName(this.term).subscribe({
       next: (countries) => {
         this.countries = countries;
@@ -29,7 +32,16 @@ export class ByCountryComponent {
   }
 
   public getSuggestions(term: string): void {
+    this.term = term;
     this.error = false;
-    console.log(term);
+    this.showSuggestions = true;
+    this.countriesService.searchByName(term).subscribe({
+      next: (countries) => {
+        this.suggestions = countries.slice(0, 5);
+      },
+      error: () => {
+        this.suggestions = [];
+      },
+    });
   }
 }
