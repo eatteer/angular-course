a1cb705ac2c64c567b117a31ca4a77804fdc540f
Practicante Programacion TI <practicante.ti2@austinreed.com.co>
2023-01-27 09:32:28 -0500

    Implement dynamic and switches forms using reactive forms

--- 06-forms/src/app/reactive/pages/dynamics/dynamics.component.ts
+++ 06-forms/src/app/reactive/pages/dynamics/dynamics.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+interface FormSchema {
+  name: FormControl<string | null>;
+  favoriteGame: FormControl<string | null>;
+  favoriteGames: FormArray<FormControl<string | null>>;
+}
 
 @Component({
   selector: 'app-dynamics',
@@ -8,10 +20,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class DynamicsComponent {
   public form: FormGroup = this.fb.group({
-    name: [null, [Validators.required, Validators.minLength(3)]],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGame: [''],
+    favoriteGames: this.fb.array(
+      [
+        ['Metal Gear', [Validators.required]],
+        ['Death Stranding', [Validators.required]],
+      ],
+      [Validators.required]
+    ),
   });
 
   public constructor(private fb: FormBuilder) {}
+
+  public get favoriteGames(): FormArray {
+    return this.form.get('favoriteGames') as FormArray;
+  }
+
+  public addFavoriteGame(): void {
+    const control = this.form.controls['favoriteGame'];
+    if (control.invalid) return;
+    const { value } = control;
+    const newControl = new FormControl(value, [Validators.required]);
+    this.favoriteGames.push(newControl);
+    control.reset();
+  }
+
+  public removeFavoriteGame(index: number): void {
+    this.favoriteGames.removeAt(index);
+  }
 
   public logForm(): void {
     console.log(this.form);
--- 06-forms/src/app/reactive/pages/switches/switches.component.ts
+++ 06-forms/src/app/reactive/pages/switches/switches.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class SwitchesComponent {
+export class SwitchesComponent implements OnInit {
+  public form: FormGroup = this.fb.group({
+    genre: ['female', [Validators.required]],
+    notifications: [true, [Validators.required]],
+    terms: [false, [Validators.requiredTrue]],
+  });
 
+  public person = {
+    genre: 'male',
+    notifications: false,
+    terms: true,
+  };
+
+  public constructor(private fb: FormBuilder) {}
+
+  public ngOnInit(): void {
+    this.form.reset({ ...this.person });
+
+    // Update person values on form value changes
+    this.form.valueChanges.subscribe((values) => {
+      this.person = values;
+    });
+  }
+
+  public submit(): void {
+    // this.person = this.form.value;
+  }
 }
