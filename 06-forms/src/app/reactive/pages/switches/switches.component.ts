7bdaf0d9752f9993b5eed3d5ed36052838d52fc8
Practicante Programacion TI <practicante.ti2@austinreed.com.co>
2023-01-27 11:26:10 -0500

    Implement multiple checkbox using reactive forms

--- 06-forms/src/app/reactive/pages/switches/switches.component.ts
+++ 06-forms/src/app/reactive/pages/switches/switches.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -7,10 +13,18 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styles: [],
 })
 export class SwitchesComponent implements OnInit {
+  public favoriteVehicles = [
+    { name: 'Bike', id: 1 },
+    { name: 'Car', id: 2 },
+    { name: 'Boat', id: 3 },
+    { name: 'Airplane', id: 4 },
+  ];
+
   public form: FormGroup = this.fb.group({
     genre: ['female', [Validators.required]],
     notifications: [true, [Validators.required]],
     terms: [false, [Validators.requiredTrue]],
+    favoriteVehicles: this.fb.array([], [Validators.required]),
   });
 
   public person = {
@@ -28,6 +42,20 @@ export class SwitchesComponent implements OnInit {
     this.form.valueChanges.subscribe((values) => {
       this.person = values;
     });
+  }
+
+  public onChangeCheckbox(event: Event): void {
+    const checkbox = event.target as HTMLInputElement;
+    const { checked } = checkbox;
+    const value = Number(checkbox.value);
+    const fv = this.form.controls['favoriteVehicles'] as FormArray;
+    if (checked) {
+      const control = new FormControl(value);
+      fv.push(control);
+    } else {
+      const i = fv.controls.findIndex((control) => control.value === value);
+      fv.removeAt(i);
+    }
   }
 
   public submit(): void {
