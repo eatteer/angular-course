7f33352ad36281be730bc580a392fb9b933cafa6
Practicante Programacion TI <practicante.ti2@austinreed.com.co>
2023-02-06 16:20:22 -0500

    Remove navora from angular-course

--- navora/src/app/maps/pages/map-page/map-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-map-page',
-  templateUrl: './map-page.component.html',
-  styles: [],
-})
-export class MapPageComponent {}
